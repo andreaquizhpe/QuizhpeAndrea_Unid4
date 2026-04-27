ea3bacb420a01994861ffc2c23ef321c47ebb366
andreaquizhpe <belenquizhpe@gmail.com>
2026-04-26 21:14:23 -0500

    (Creacion de la clase Actor y la relacion con pelicula)

---
+++ src/uni1a/Investigador.java
@@ -0,0 +1,5 @@
+package uni1a;
+
+public class Investigador {
+
+}
--- src/uni1a/Pelicula.java
+++ src/uni1a/Pelicula.java
@@ -3,13 +3,19 @@
  */
 package uni1a;
 
+import java.util.*;
+
 // Subclase Pelicula que extiende de ContenidoAudiovisual
 public class Pelicula extends ContenidoAudiovisual {
     private String estudio;
+    
+    //Relacion con Pelicula por asociacion
+    private List<Actor> actores;
 
     public Pelicula(String titulo, int duracionEnMinutos, String genero, String estudio) {
         super(titulo, duracionEnMinutos, genero);
         this.estudio = estudio;
+        this.actores = new ArrayList<Actor>();
     }
 
     public String getEstudio() {
@@ -29,5 +35,20 @@ public class Pelicula extends ContenidoAudiovisual {
         System.out.println("Género: " + getGenero());
         System.out.println("Estudio: " + estudio);
         System.out.println();
+    }
+    //metodo para agregar actores
+    public void agregarActor(Actor actor) {
+    	if (!actores.contains(actor)) {
+    		actores.add(actor);
+		}else
+			System.out.println("El actor ya existe");
+    	
+    }
+    //Metodo para mostrar la lista de actores de la pelicula
+    public void mostrarActores() {
+    	for (Actor actor: actores) {
+    		System.out.println(actor.getNombre() + actor.getEdad());
+			
+		}
     }
 }
